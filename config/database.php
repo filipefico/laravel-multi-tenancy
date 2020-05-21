0ed6190671b57eb1beb9408eb20e874cc15b3514
Filipe Fico <filipefico@gmail.com>
2020-05-20 21:01:14 -0300

    Criando a conexão system para o banco de dados

--- config/database.php
+++ config/database.php
@@ -91,6 +91,26 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'system' => [
+            'driver' => 'mysql',
+            'url' => env('SYSTEM_DATABASE_URL'),
+            'host' => env('SYSTEM_DB_HOST', '127.0.0.1'),
+            'port' => env('SYSTEM_DB_PORT', '3306'),
+            'database' => env('SYSTEM_DB_DATABASE', 'forge'),
+            'username' => env('SYSTEM_DB_USERNAME', 'forge'),
+            'password' => env('SYSTEM_DB_PASSWORD', ''),
+            'unix_socket' => env('SYSTEM_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
